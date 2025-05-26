6a54220332c93ea9403185f02960e4598c973e3e
Phillipe <ge.bonhomme@duocuc.cl>
2025-05-26 16:50:32 -0400

    Debido a un breakdown mental, guardo cambios hasta aqui para tomarme un break

---
+++ src/main/java/com/bamboo/cursosdeestudiante/Model/Reporte.java
@@ -0,0 +1,28 @@
+package com.bamboo.cursosdeestudiante.Model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class Reporte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(length = 50, nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private LocalDateTime fecha;
+
+}
